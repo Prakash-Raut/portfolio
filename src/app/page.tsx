52047045751742442c690128c1c4dca176889c21
Prakash Raut <prakash_raut@outlook.com>
2024-09-13 17:33:41 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,107 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Github, Linkedin, Twitter } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default function Portfolio() {
+	return (
+		<div className="min-h-screen p-8 max-w-[640px] mx-auto">
+			<header className="flex items-center space-x-4 mb-8">
+				<Image
+					src="/avatar.jpg"
+					alt="John Rush"
+					width={64}
+					height={64}
+					className="rounded-full"
+				/>
+				<div>
+					<h1 className="text-xl font-bold">Prakash Raut</h1>
+					<p className="text-sm text-muted-foreground">
+						100x software engineer
+					</p>
+				</div>
+				<div className="flex space-x-2">
+					<Button
+						asChild
+						variant="outline"
+					>
+						<Link
+							href="https://github.com/Prakash-Raut"
+							className="px-3 py-1 rounded text-sm"
+							target="_blank"
+						>
+							<Github className="h-4 w-4" />
+						</Link>
+					</Button>
+					<Button
+						asChild
+						variant="outline"
+					>
+						<Link
+							href="https://www.linkedin.com/in/prakashkumarraut/"
+							className="px-3 py-1 rounded text-sm"
+						>
+							<Linkedin className="h-4 w-4" />
+						</Link>
+					</Button>
+					<Button
+						asChild
+						variant="outline"
+					>
+						<Link
+							href="https://x.com/Mr_Prakash_Raut"
+							className="px-3 py-1 rounded text-sm"
+						>
+							<Twitter className="h-4 w-4" />
+						</Link>
+					</Button>
+				</div>
+			</header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+			<div className="space-y-2 mb-8">
+				<blockquote className="mt-6 border-l-2 pl-6 italic">
+					There is no secret ingredient.
+				</blockquote>
+			</div>
+
+			<section className="mb-8">
+				<h2 className="scroll-m-20 text-xl font-semibold tracking-tight">
+					Projects
+				</h2>
+				{[
+					{
+						name: "Leetcode clone",
+						description:
+							"Expand your knowledge and prepare for technical interviews.",
+						link: "https://github.com/Prakash-Raut/LeetCode",
+					},
+					{
+						name: "Uber",
+						description:
+							"Get a ride in minutes. Or become a driver and earn money on your schedule.",
+						link: "https://github.com/Prakash-Raut/Uber-App",
+					},
+				].map((project) => (
+					<Link
+						href={project.link}
+						key={project.name}
+						className="flex justify-between items-center py-3 my-2 px-4 rounded-xl"
+						target="_blank"
+					>
+						<div>
+							<p className="text-sm font-medium leading-none">
+								{project.name}
+							</p>
+							<small className="font-medium text-sm text-muted-foreground">
+								{project.description}
+							</small>
+						</div>
+						<ArrowRight className="h-4 w-4" />
+					</Link>
+				))}
+			</section>
+
+			<footer className="flex justify-between items-center pt-8 mt-8"></footer>
+		</div>
+	);
 }
